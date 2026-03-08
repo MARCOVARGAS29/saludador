7eabfe3d718454d670eebb20c6bd7dde5441193d
Marco Vargas <jhean.vargas@ucb.edu.bo>
2026-03-08 12:02:35 -0400

    feat: implementar saludo dinámico según la hora del día

--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,16 @@
 function saludar(nombre) {
-  return "Hola " + nombre;
+  const hora = new Date().getHours();
+  let saludo = "";
+
+  if (hora >= 5 && hora < 12) {
+    saludo = "Buenos días";
+  } else if (hora >= 12 && hora < 20) {
+    saludo = "Buenas tardes";
+  } else {
+    saludo = "Buenas noches";
+  }
+
+  return saludo + " " + nombre;
 }
 
 export default saludar;
