9dc2e5006064f99b25d631b3409ba9a8a1882a1b
Marco Vargas <jhean.vargas@ucb.edu.bo>
2026-03-08 11:58:33 -0400

    feat: agregar saludo personalizado por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -2,8 +2,11 @@ import saludar from "./saludador";
 
 const boton = document.querySelector("#botonsaludar");
 const divResultado = document.querySelector("#resultado-div");
+const inputNombre = document.querySelector("#nombre"); // Capturamos el input
 
 boton.addEventListener("click", (event) => {
   event.preventDefault();
-  divResultado.innerHTML = "<p>" + saludar() + "</p>";
+  
+  const nombre = inputNombre.value; // Obtenemos el texto
+  divResultado.innerHTML = "<p>" + saludar(nombre) + "</p>";
 });
